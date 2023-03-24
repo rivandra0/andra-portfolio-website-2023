b84a1cd791000023fbc88f9d6c82f562c6880871
Muhammad Rivandra <rivandra0@gmail.com>
2023-03-24 23:55:36 +0700

    first commit

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,22 @@
+<template>
+  <q-page>
+    <HeroHead/>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+import HeroHead from 'src/components/HeroHead.vue'
+
+export default defineComponent({
+  name: 'IndexPage',
+  components: {
+    HeroHead
+  },
+  setup () {
+    // const pixiContainer = ref(null)
+    // const app = null
+
+  }
+})
+</script>

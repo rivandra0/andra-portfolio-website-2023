f81bc44052b365981a31767c2182a3cd11d5daca
Muhammad Rivandra <rivandra0@gmail.com>
2023-04-02 14:11:54 +0700

    refactoring social buttons

--- src/components/SocialButtons.vue
+++ src/components/SocialButtons.vue
@@ -1,25 +1,48 @@
 <template>
   <div class="q-my-xl">
-    <div class="q-pa-sm">Or contact me via my social media: </div>
-    <a href="https://www.linkedin.com/in/muhammad-rivandra-620277202?originalSubdomain=id" target="_blank">
-      <q-btn class="q-mr-md" round outline color="primary" icon="mdi-linkedin" size="md"/>
-    </a>
-    <a href="https://www.instagram.com/rivandram/?hl=en" target="_blank">
-      <q-btn class="q-mr-md" round outline color="red" icon="mdi-instagram" size="md" />
-    </a>
-    <a href="https://github.com/TuyulMohak" target="_blank">
-      <q-btn round outline color="black" icon="mdi-github" size="md"/>
-    </a>
-    <a href="mailto:rivandra0@gmail.com" target="_blank">
-      <q-btn round outline color="primary" icon="mdi-email-outline" size="md"/>
-    </a>
+    <div class="q-pa-sm q-gutter-x-sm">Or contact me via my social media: </div>
+    <q-btn
+      v-for="data in socialData"
+      :key="data.url"
+      :icon="data.icon"
+      :href="data.url"
+      :color="data.color"
+      target="_blank"
+      size="md"
+    />
   </div>
 </template>
 
 <script>
+
+const socialData = [
+  {
+    icon: 'mdi-linkedin',
+    url: 'https://www.linkedin.com/in/muhammad-rivandra-620277202?originalSubdomain=id',
+    color: 'primary'
+  },
+  {
+    icon: 'mdi-instagram',
+    url: 'https://www.instagram.com/rivandram/?hl=en',
+    color: 'red'
+  },
+  {
+    icon: 'mdi-github',
+    url: 'https://github.com/TuyulMohak',
+    color: 'black'
+  },
+  {
+    icon: 'mdi-email-outline',
+    url: 'mailto:rivandra0@gmail.com',
+    color: 'orange'
+  }
+]
+
 export default {
   setup () {
-    return {}
+    return {
+      socialData
+    }
   }
 }
 </script>

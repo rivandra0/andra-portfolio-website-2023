da0d4f372b7eb248589bf1776c141bf4b13de937
Muhammad Rivandra <rivandra0@gmail.com>
2023-04-15 17:37:20 +0700

    removing the redirects file, useless

--- src/router/routes.js
+++ src/router/routes.js
@@ -18,6 +18,10 @@ const routes = [
   {
     path: '/:catchAll(.*)*',
     component: () => import('pages/ErrorNotFound.vue')
+  },
+  {
+    path: '*',
+    component: () => import('pages/IndexPage.vue')
   }
 ]
 
